68d20a12ad19ccea5a3dea5f3ea15c5efd316ea8
Tiago Magalhaes <tmagalhaes@lugaradajoia.com>
2013-10-30 23:47:43 +0000

    30_10_13_2

--- 2.An_Introduction_to_Interactive_Programming_in_Python/teste.py
+++
@@ -1,7 +0,0 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1265
-{\fonttbl\f0\fnil\fcharset0 Cochin;}
-{\colortbl;\red255\green255\blue255;}
-\deftab560
-\pard\tx560\tx1120\tx1680\tx2240\tx2800\tx3360\tx3920\tx4480\tx5040\tx5600\tx6160\tx6720\pardeftab560\sl336\slmult1\pardirnatural
-
-\f0\fs34 \cf0 teste}
